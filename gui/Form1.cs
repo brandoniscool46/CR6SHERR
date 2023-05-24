37be6a64570a1e271747c939a2f3819fa6e70e9d
mgXzyy <107469245+mariangXzyy@users.noreply.github.com>
2023-05-24 11:02:45 +0300

    uploaded incomplete gui version

---
+++ gui/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gui
+{
+    public partial class cr6sherrgui : Form
+    {
+        bool isMouseDown;
+        public cr6sherrgui()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void topbar_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+        }
+
+        private void topbar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                int mousex = MousePosition.X - 400;
+                int mousey = MousePosition.Y - 20;
+                this.SetDesktopLocation(mousex, mousey);
+            }
+        }
+
+        private void topbar_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://github.com/mariangXzyy/CR6SHERR");
+        }
+    }
+}
